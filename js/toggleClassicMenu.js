efc30f32e8fcfadcd72564f725f2ec1be6095731
jedartbywork <mhaker2004@gmail.com>
2023-07-04 18:40:47 +0600

    Some fixes form H3C

--- js/toggleClassicMenu.js
+++
@@ -1,6 +0,0 @@
-export function toggleClassicMenu() {
-    headerWrapper.classList.toggle("header__wrapper_classic");
-    classicMenu.classList.toggle('header_flex');
-    bodyEl.classList.toggle('body_classic')
-    toggleContentBlocks();
-}
